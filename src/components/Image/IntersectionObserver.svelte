8f9d5e3c0b61e69cc6f22a75bde6dd6fcf381556
Chris McIntosh <5742606+chris-mcintosh@users.noreply.github.com>
2024-11-20 21:43:24 -0500

    Set rootMargin to 150px to load the next row of images

--- src/components/Image/IntersectionObserver.svelte
+++ src/components/Image/IntersectionObserver.svelte
@@ -1,16 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
 	export let once = false;
-	export let top = 0;
-	export let bottom = 0;
-	export let left = 0;
-	export let right = 0;
   let intersecting = false;
   
 	let container;
 	onMount(() => {
 		if (typeof IntersectionObserver !== 'undefined') {
-			const rootMargin = `${bottom}px ${left}px ${top}px ${right}px`;
+			//const rootMargin = `${bottom}px ${left}px ${top}px ${right}px`;
+			const rootMargin = `150px`;
 			const observer = new IntersectionObserver(entries => {
 				intersecting = entries[0].isIntersecting;
 				if (intersecting && once) {
@@ -22,20 +19,6 @@
 			observer.observe(container);
 			return () => observer.unobserve(container);
 		}
-		// function handler() {
-		// 	const bcr = container.getBoundingClientRect();
-		// 	intersecting = (
-		// 		(bcr.bottom + bottom) > 0 &&
-		// 		(bcr.right + right) > 0 &&
-		// 		(bcr.top - top) < window.innerHeight &&
-		// 		(bcr.left - left) < window.innerWidth
-		// 	);
-		// 	if (intersecting && once) {
-		// 		window.removeEventListener('scroll', handler);
-		// 	}
-		// }
-		// window.addEventListener('scroll', handler);
-		// return () => window.removeEventListener('scroll', handler);
 	});
 </script>
 
